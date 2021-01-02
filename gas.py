56c6725f94a18c548ab8126d21a2cb6ed23507ab
Kike00 <kike.lopez@opendeusto.es>
2021-01-02 17:46:29 +0100

    gas, buzzer, video stream

---
+++ gas.py
@@ -0,0 +1,49 @@
+import math
+import sys
+import time
+from grove.adc import ADC
+# from influxdb import InfluxDBClient
+ 
+class GroveGasSensorMQ2:
+ 
+    def __init__(self, channel):
+        self.channel = channel
+        self.adc = ADC()
+ 
+    @property
+    def MQ2(self):
+        value = self.adc.read(self.channel)
+        return value
+ 
+Grove = GroveGasSensorMQ2
+sensor = GroveGasSensorMQ2(0)  
+ 
+def getGasValue(port):
+    # sensor = GroveGasSensorMQ2(port)   
+    return(sensor.MQ2)
+
+ 
+def main():
+    # client = InfluxDBClient(host='localhost', port=8086, database='home')
+    if len(sys.argv) < 2:
+        print('Usage: {} adc_channel'.format(sys.argv[0]))
+        sys.exit(1)
+ 
+    sensor = GroveGasSensorMQ2(int(sys.argv[1]))
+ 
+    print('Detecting...')
+    while True:
+        print('Gas value: {0}'.format(sensor.MQ2))
+        # json_body = [
+        # {
+        #     "measurement": "gas_value",
+        #     "fields": {
+        #         "value": sensor.MQ2
+        #     }
+        # }
+        # ]
+        # client.write_points(json_body)
+        time.sleep(5)
+ 
+if __name__ == '__main__':
+    main()
---
+++ motion.py
@@ -0,0 +1,51 @@
+import time
+from grove.gpio import GPIO
+ 
+ 
+class GroveMiniPIRMotionSensor(GPIO):
+    def __init__(self, pin):
+        super(GroveMiniPIRMotionSensor, self).__init__(pin, GPIO.IN)
+        self._on_detect = None
+ 
+    @property
+    def on_detect(self):
+        return self._on_detect
+ 
+    @on_detect.setter
+    def on_detect(self, callback):
+        if not callable(callback):
+            return
+ 
+        if self.on_event is None:
+            self.on_event = self._handle_event
+ 
+        self._on_detect = callback
+ 
+    def _handle_event(self, pin, value):
+        if value:
+            if callable(self._on_detect):
+                self._on_detect()
+ 
+Grove = GroveMiniPIRMotionSensor
+ 
+ 
+def main():
+    import sys
+ 
+    if len(sys.argv) < 2:
+        print('Usage: {} pin'.format(sys.argv[0]))
+        sys.exit(1)
+ 
+    pir = GroveMiniPIRMotionSensor(int(sys.argv[1]))
+ 
+    def callback():
+        print('Motion detected.')
+ 
+    pir.on_detect = callback
+ 
+    while True:
+        time.sleep(1)
+ 
+ 
+if __name__ == '__main__':
+    main()
